ad757080adbe56228c46e704f4f224bb01af4a3d
Joshua <Joshua.tomos.james@gmail.com>
2024-09-21 23:00:08 +0100

    Fix unused imports (stopping from building)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import { GetStaticProps } from 'next';
-import prisma from '../lib/prisma';
 import Image from "next/image";
 
 const Home = async() => {
-
-  const data = await prisma.user.findFirst({});
-  console.log(data);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,8 +3,10 @@ import { PrismaClient } from '@prisma/client';
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
+  // In production, we want to create a new instance of PrismaClient.
   prisma = new PrismaClient();
 } else {
+  // In development, we want to reuse the same instance across hot-reloads.
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
