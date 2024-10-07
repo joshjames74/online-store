c8ef200e625b9f54a021e644a61c1da0e00797d7
Joshua <Joshua.tomos.james@gmail.com>
2024-10-07 17:23:35 +0100

    Solve bigint bug

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,15 +1,17 @@
 import { postHelper } from '@/api/helpers/request';
 import { postProduct } from '@/api/services/productService';
 import { Product } from '@prisma/client';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 
 // POST request
 
-export async function POST(req: Request): Promise<NextResponse> {
+export async function POST(req: NextRequest): Promise<NextResponse> {
+
 
   const body = await req.json();
-  const product: Omit<Product, 'product_id'> = body;
+  let product: Omit<Product, 'product_id'> = body;
+
 
   return await postHelper('product', postProduct, product);
 }
--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,34 +1,27 @@
-import { PrismaClient } from '@prisma/client';
-import { readFile } from 'fs/promises';
+const { PrismaClient } = require('@prisma/client');
+const { readFile } = require('fs/promises');
 
 const prisma = new PrismaClient();
 
 async function main() {
 
-  // // Delete existing data to start fresh
-  // await prisma.country.deleteMany();
-  // await prisma.currency.deleteMany();
-  // await prisma.user.deleteMany();
-  // await prisma.product.deleteMany();
-  // await prisma.category.deleteMany();
-  // await prisma.address.deleteMany();
-  // await prisma.order.deleteMany();
-  // await prisma.basket.deleteMany();
-
-  // to do: reset seed
-  const truncate_all_tables = await readFile('../online-store/src/database/truncate-all-tables.sql', { encoding: 'utf8'});
-  await prisma.$executeRawUnsafe(truncate_all_tables);
-
-
+  await prisma.$executeRaw`TRUNCATE TABLE Country RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE Currency RESTART IDENTITY CASCADE`;
+  //await prisma.$executeRaw`TRUNCATE TABLE User RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE Category RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE Product RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE Address RESTART IDENTITY CASCADE`;
+  //await prisma.$executeRaw`TRUNCATE TABLE Order RESTART IDENTITY CASCADE`;
+  //await prisma.$executeRaw`TRUNCATE TABLE BasketItem RESTART IDENTITY CASCADE`;
+  //await prisma.$executeRaw`TRUNCATE TABLE OrderItem RESTART IDENTITY CASCADE`;
+  
   // set up sql files
-  const country_sql = await readFile('../online-store/src/database/country.sql', { encoding: 'utf8'});
-  const currency_sql = await readFile('../online-store/src/database/currency.sql', { encoding: 'utf8'});
+  const country_sql = await readFile('../database/country.sql', { encoding: 'utf8'});
+  const currency_sql = await readFile('../database/currency.sql', { encoding: 'utf8'});
 
   // Add known seed data
   await prisma.$executeRawUnsafe(country_sql);
   await prisma.$executeRawUnsafe(currency_sql);
-
-  // await prisma.currency.create({data: {code: "GBP", symbol: "£", gbp_exchange_rate: 1}})
 
 }
 
