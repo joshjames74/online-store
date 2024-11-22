591d2f2265da117706f9e4d6233fdb2995a1612a
Joshua <Joshua.tomos.james@gmail.com>
2024-11-22 12:26:54 +0000

    Add basket item count to basket button

--- src/components/navigation/basket-button.tsx
+++ src/components/navigation/basket-button.tsx
@@ -1,11 +1,45 @@
+"use client";
+import { getBasketByUserId } from "@/api/request/basketRequest";
+import { UserContext } from "@/contexts/user-context";
 import { ShoppingCartOutlined } from "@ant-design/icons";
-import { Button, Link } from "@chakra-ui/react";
+import { Button, HStack, Link, Text } from "@chakra-ui/react";
+import { useContext, useEffect, useState } from "react";
 
 export default function BasketButton(): JSX.Element {
+
+  const { user } = useContext(UserContext);
+  const [count, setCount] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loadCount = () => {
+    if (!user || !user.id) {
+      return;
+    }
+    setIsLoading(true);
+    getBasketByUserId(user.id).then((res) => {
+      setCount(res.metadata.count);
+    }).catch((error) => {
+      console.error(error);
+    }).finally(() => {
+      setIsLoading(false);
+    })
+  }
+
+  useEffect(() => {
+    loadCount();
+  }, []);
+
+  useEffect(() => {
+    loadCount();
+  }, [user])
+
   return (
     <Link href={"/user/basket"}>
       <Button fontSize="2xl">
-        <ShoppingCartOutlined />
+        <HStack>
+          <ShoppingCartOutlined />
+          <Text display={count && count > 0 ? "block" : "none"} fontSize="md">({count && count > 0 ? count : <></>})</Text>
+        </HStack>
       </Button>
     </Link>
   );
